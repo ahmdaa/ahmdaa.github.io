51bc5496c5f7b9973899b238162ef6cce17cc54a
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-10-09 20:55:09 -0400

    Set up basic site with sass styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import './App.css';
+import './sass/main.scss';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 function App() {
     return (
         <div>
-            <h1>Hello World!</h1>
-            <p>My name is Ahmed!</p>
+            <header>
+                <h1>Hi, I'm Ahmed!</h1>
+            </header>
+            <div className='socials'>
+                <div>
+                    <a className='social-link' href="https://github.com/ahmdaa">
+                        <AiFillGithub />
+                        <div className='social-text'>
+                            Github
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a className='social-link' href="https://linkedin.com/in/ahmed-abdalla-31a030142">
+                        <AiFillLinkedin />
+                        <div className='social-text'>
+                            LinkedIn
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
     );
 }
